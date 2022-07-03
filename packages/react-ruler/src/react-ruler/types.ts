9d55c0fad46e221fcd6e32fb9b54418eca723ad6
Daybrush <daybrush@gmail.com>
2022-07-03 19:38:03 +0900

    docs: change docs

--- packages/react-ruler/src/react-ruler/types.ts
+++ packages/react-ruler/src/react-ruler/types.ts
@@ -5,58 +5,152 @@ import { IObject } from "@daybrush/utils";
  * @memberof Ruler
  */
 export interface RulerInterface {
+    /**
+     * Scroll to that location. It can be used when the scrollPos prop is not used.
+     * @param - scroll position
+     */
     scroll(scrollPos: number): void;
+    /**
+     * Recalculate the style of the ruler.
+     */
     resize(): void;
 }
 
 /**
  * @typedef
  * @memberof Ruler
- * @property - ruler's direction
- * @property - ruler's width (default: offsetWidth)
- * @property - ruler's height (default: offsetheight)
- * @property - main scale unit (default: 50)
- * @property - zoom (default: 1)
- * @property - line's direction (default: "end")
- * @property - text align (default: "start")
- * @property - font and size  (default: "10px sans-serif")
- * @property - Number of areas to divide between two main lines (default: 10)
- * @property - main line size (default "100%")
- * @property - long line size (default: 10)
- * @property - short line size (default: 7)
- * @property - text's position offset (default: [0, 0])
- * @property - Whether to display the ruler in the negative direction (default: true)
- * @property - The range to show the ruler's scales (default: [-Infinity, Infinity])
- * @property - you can set scroll position by prop. If you use the scrollPos prop, you can't use the scroll method. (default: 0)
- * @property - ruler canvas style
- * @property - ruler's background color (default: "#333333")
- * @property - ruler's line color (default: "#777777")
- * @property - ruler's text color (default: "#ffffff")
- * @property - ruler's text format
  */
 export interface RulerProps {
+    /**
+     * ruler's direction
+     * @default "horizontal"
+     */
     type?: "horizontal" | "vertical";
+    /**
+     * ruler's width
+     * @default target's offsetWidth
+     */
     width?: number;
+    /**
+     * ruler's height
+     * @default target's offsetHeight
+     */
     height?: number;
+    /**
+     * main scale unit
+     * @default 50
+     * @example
+     * ```
+     * 1px (Default) zoom: 1, unit: 50 (every 50px)
+     * 1cm = 37.7952px zoom: 37.7952 unit: 1 (every 1cm)
+     * 1in = 96px = 2.54cm zoom: 9 unit: 1 (every 1in)
+     * ```
+     */
     unit?: number;
+    /**
+     * zoom
+     * @default 1
+     * @example
+     * ```
+     * 1px (Default) zoom: 1, unit: 50 (every 50px)
+     * 1cm = 37.7952px zoom: 37.7952 unit: 1 (every 1cm)
+     * 1in = 96px = 2.54cm zoom: 9 unit: 1 (every 1in)
+     * ```
+     */
     zoom?: number;
+    /**
+     * line's direction (align)
+     * @default "end"
+     */
     direction?: "start" | "center" | "end";
+    /**
+     * text align
+     * @default "start"
+     */
     textAlign?: "left" | "center" | "right";
+    /**
+     * font and size
+     * @default "10px sans-serif"
+     */
     font?: string;
+    /**
+     * Number of areas to divide between two main lines
+     * @default 10
+     */
     segment?: number;
+    /**
+     * main line size
+     * @default "100%"
+     */
     mainLineSize?: string | number;
+    /**
+     * long line size
+     * @default 10
+     */
     longLineSize?: string | number;
+    /**
+     * short line size
+     * @default 7
+     */
     shortLineSize?: string | number;
+    /**
+     * line's position offset
+     * @default [0, 0]
+     */
     lineOffset?: number[];
+    /**
+     * text's position offset
+     * @default [0, 0]
+     */
     textOffset?: number[];
+    /**
+     * Whether to display the ruler in the negative direction.
+     * @default true
+     */
     negativeRuler?: boolean;
+    /**
+     * The range to show the ruler's scales
+     * @default [-Infinity, Infinity]
+     */
     range?: [number, number];
+    /**
+     * you can set scroll position by prop.
+     * If you use the scrollPos prop, you can't use the scroll method.
+     * @default undefined
+     */
     scrollPos?: number;
+    /**
+     * ruler canvas style
+     */
     style?: IObject<any>;
+    /**
+     * ruler's background color
+     * @default "#333333"
+     */
     backgroundColor?: string;
+    /**
+     * background color within range in ruler
+     * @default "transparent"
+     */
     rangeBackgroundColor?: string;
+    /**
+     * ruler's line color
+     * @default "#777777"
+     */
     lineColor?: string;
+    /**
+     * ruler's text color
+     * @default "#ffffff"
+     */
     textColor?: string;
+    /**
+     * The background color that wraps the text
+     * @default "transparent"
+     */
     textBackgroundColor?: string;
+    /**
+     * text foramt
+     * @default ownself
+     */
     textFormat?: (scale: number) => string;
 }
